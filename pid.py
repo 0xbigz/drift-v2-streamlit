7af7478a010409723b7522d3dc29eabef24a6786
0xbigz <83473873+0xbigz@users.noreply.github.com>
2022-10-26 18:03:46 -0400

    update

--- pid.py
+++ pid.py
@@ -1,22 +1,49 @@
 
 import sys
-sys.path.insert(0, './driftpy/src/')
-
+import driftpy
 import pandas as pd 
 import numpy as np 
 
 pd.options.plotting.backend = "plotly"
 
-from driftpy.constants.config import configs
+print(driftpy.__dir__())
+# from driftpy.constants.config import configs
 from anchorpy import Provider, Wallet
 from solana.keypair import Keypair
 from solana.rpc.async_api import AsyncClient
 from driftpy.clearing_house import ClearingHouse
-from driftpy.accounts import *
+from driftpy.accounts import get_perp_market_account, get_spot_market_account, get_user_account, get_state_account
 from driftpy.constants.numeric_constants import * 
 import os
 import json
 import streamlit as st
+from driftpy.constants.banks import devnet_banks, Bank
+from driftpy.constants.markets import devnet_markets, Market
+from dataclasses import dataclass
+from solana.publickey import PublicKey
+
+
+@dataclass
+class Config:
+    env: str
+    pyth_oracle_mapping_address: PublicKey
+    clearing_house_program_id: PublicKey
+    usdc_mint_address: PublicKey
+    markets: list[Market]
+    banks: list[Bank]
+
+
+configs = {
+    "devnet": Config(
+        env='devnet',
+        pyth_oracle_mapping_address=PublicKey('BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2'),
+		clearing_house_program_id=PublicKey('jAEeKs9twxAJmXZHqS2p459xW7FMDjoyvuqthRo9qGS'),
+		usdc_mint_address=PublicKey('8zGuJQqwhZafTah7Uc7Z4tXRnguqkn5KLFAP8oV6PHe2'),
+		markets=devnet_markets,
+		banks=devnet_banks,
+    )
+}
+
 
 async def show_pid_positions(pid='', url='https://api.devnet.solana.com'):
     config = configs['devnet']
