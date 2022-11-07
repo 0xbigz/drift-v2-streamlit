f4b8a01bb295f7040316775cbd23e78c72a680f6
0xNineteen <0x39015319@gmail.com>
2022-11-07 23:11:44 +0000

    add timestamps to logs

--- logs.py
+++ logs.py
@@ -73,7 +73,11 @@ async def get_all(limit, endpoint):
         def call_b(evt): 
             logs[sig] = logs.get(sig, []) + [evt]
         parser.parse_logs(tx['result']['meta']['logMessages'], call_b)
- 
+
+    log_times = list(set([event.data.ts for events in logs.values() for event in events]))
+    max_log = max(log_times)
+    min_log = min(log_times)
+
     log_names = list(set([event.name for events in logs.values() for event in events]))
     type_to_log = {}
     for log_name in log_names:
@@ -84,7 +88,7 @@ async def get_all(limit, endpoint):
                         type_to_log[log_name] = {}
                     type_to_log[log_name][sig] = type_to_log[log_name].get(sig, []) + [event.data]
 
-    return log_names, type_to_log, n_logs
+    return log_names, type_to_log, n_logs, (max_log, min_log)
 
 async def view_logs(url):
     config = configs['mainnet-beta']
@@ -101,7 +105,12 @@ async def view_logs(url):
     # log liquidations
     # ...
     limit = st.number_input('tx look up limit', value=10)
-    log_names, type_to_log, n_logs = await get_all(limit, url)
+    log_names, type_to_log, n_logs, (max_ts, min_ts) = await get_all(limit, url)
+    
+    import datetime
+    min_ts = datetime.datetime.fromtimestamp(min_ts)
+    max_ts = datetime.datetime.fromtimestamp(max_ts)
+    st.write(f'min <-> max ts :: {min_ts} <-> {max_ts}')
 
     options = st.multiselect('Log types', log_names, log_names)
     st.write(f'### number of logs found: {n_logs}')
