13df677c80d0c6e0f382dcea1183bb988d54fe6a
0xNineteen <0x39015319@gmail.com>
2022-11-10 10:27:53 +0000

    optimize batch log requests

--- logs.py
+++ logs.py
@@ -36,30 +36,92 @@ def get_account_txs(pk, limit=10, last_hash=None):
         resp = requests.get(f'https://public-api.solscan.io/account/transactions?account={pk}&limit={limit}&beforeHash={last_hash}')
     return resp
 
+# 150 requests/ 30 seconds
 def get_last_n_tx_sigs(program_id, limit):
     sigs = []
     last_hash = None
+    n_requests = 0
     # sol scan only allows 50 at a time
     for i in range(0, limit, 50):
-        amount = 50 if i < limit else limit % 50 
+        amount = 50 if i+50 < limit else limit % 50 
         resp = get_account_txs(program_id, amount, last_hash)
         resp = json.loads(resp.text)
         sigs += [r['txHash'] for r in resp]
+        if len(resp) == 0:
+            return sigs, len(sigs)
+
         last_hash = sigs[-1]
+        n_requests += 1
     
-    return sigs
+    return sigs, n_requests
+
+def get_tx_request(sig):
+    return {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getTransaction",
+        "params": [
+            sig,
+            "json"
+        ]
+    }
+
+def batch_get_txs(sigs):
+    data = [get_tx_request(sig) for sig in sigs]
+
+    resps = []
+    step = 100
+    from tqdm import tqdm 
+    for i in tqdm(range(0, len(data), step)):
+        batch = data[i: i+step]
+        print(f'requesting {len(batch)}')
+        resp = requests.post(
+            "https://drift-cranking.rpcpool.com", 
+            headers={
+                "Content-Type": "application/json",
+                "origin": "https://app.drift.trade",
+            }, 
+            json=batch
+        )
+        try:
+            resp = json.loads(resp.text)
+        except Exception as e: 
+            print(resp.text)
+            raise e
+        
+        # expecting json list as response
+        resps += resp
+    return resp
+
+# async def query_server(ch: ClearingHouse):
+#     all_users = await ch.program.account['User'].all()
+#     page_index = 0 
+#     page_size = 1000
+
+#     jsons = []
+#     from tqdm import tqdm
+#     for x in tqdm(all_users):
+#         key = str(x.public_key)
+#         query = f'https://mainnet-beta.api.drift.trade/liquidations/userAccount/?userPublicKey={key}&pageIndex={page_index}&pageSize={page_size}'
+#         resp = requests.get(query)
+#         resp = json.loads(resp.text)
+#         if len(resp['data']['liquidations']) > 0:
+#             print(resp)
+#             jsons.append(resp)
+#             break
+#     st.json(jsons)
 
 @cached(ttl=None, cache=Cache.MEMORY)
 async def get_all(ch, limit):
-    connection = ch.program.provider.connection
-    sigs = get_last_n_tx_sigs(ch.program_id, limit)
+    sigs, n_req = get_last_n_tx_sigs(ch.program_id, limit)
+    st.write(f'n of solscan responses: {n_req}')
+    if n_req == 0: 
+        st.write('likely solscan rate limit...')
+        return False, None
 
-    promises = []
-    for sig in sigs:
-        promises.append(connection.get_transaction(sig))
-    txs = await asyncio.gather(*promises)
+    txs = batch_get_txs(sigs)
+
     n_logs = len(txs)
-    
     parser = EventParser(ch.program.program_id, ch.program.coder)
     
     logs = {}
@@ -82,13 +144,16 @@ async def get_all(ch, limit):
                         type_to_log[log_name] = {}
                     type_to_log[log_name][sig] = type_to_log[log_name].get(sig, []) + [event.data]
 
-    return log_names, type_to_log, n_logs, (max_log, min_log)
+    return True, (log_names, type_to_log, n_logs, (max_log, min_log))
 
 async def log_page(url: str, ch):
     # log liquidations
     limit = st.number_input('tx look up limit', value=10)
-    log_names, type_to_log, n_logs, (max_ts, min_ts) = await get_all(ch, limit)
-    
+    s, r = await get_all(ch, limit)
+    if not s: 
+        return
+    log_names, type_to_log, n_logs, (max_ts, min_ts) = r 
+
     import datetime
     min_ts = datetime.datetime.fromtimestamp(min_ts)
     max_ts = datetime.datetime.fromtimestamp(max_ts)
