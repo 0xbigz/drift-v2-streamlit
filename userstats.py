5a96c994aa2f445a16a9739963e95692d6154813
0xbigz <83473873+0xbigz@users.noreply.github.com>
2022-12-11 12:00:36 -0500

    add timing to user stats

--- userstats.py
+++ userstats.py
@@ -25,7 +25,7 @@ from solana.publickey import PublicKey
 from helpers import serialize_perp_market_2, serialize_spot_market
 from anchorpy import EventParser
 import asyncio
-
+import time
 async def show_user_stats(clearing_house: ClearingHouse):
     ch = clearing_house
 
@@ -40,12 +40,33 @@ async def show_user_stats(clearing_house: ClearingHouse):
     # print(df.columns)
     print(fees_df.columns)
     df = pd.concat([df, fees_df], axis=1)
-
+    print(df.columns)
     for x in df.columns:
         if x in ['taker_volume30d', 'maker_volume30d', 'filler_volume30d', 'total_fee_paid', 'total_fee_rebate']:
             df[x] /= 1e6
-    df = df[['authority', 'taker_volume30d', 'maker_volume30d', 'filler_volume30d', 'total_fee_paid', 'total_fee_rebate', 'number_of_sub_accounts']]
+    
+    current_ts = time.time()
+    print(current_ts)
+    df['last_trade_seconds_ago'] = int(current_ts) - df[['last_taker_volume30d_ts', 'last_maker_volume30d_ts']].max(axis=1).astype(int)
+    
+    
+    df = df[['authority', 'last_trade_seconds_ago', 'taker_volume30d', 'maker_volume30d', 
+    'filler_volume30d', 'total_fee_paid', 'total_fee_rebate', 
+    'number_of_sub_accounts', 'is_referrer'
+    ]].sort_values('last_trade_seconds_ago').reset_index(drop=True)
+
+    col1, col2 = st.columns(2)
+    col1.metric('30D User Taker Volume', str(np.round(df['taker_volume30d'].sum()/1e6, 2))+'M')
+    col2.metric('30D User Maker Volume', str(np.round(df['maker_volume30d'].sum()/1e6, 2))+'M')
 
     st.dataframe(df)
+
+    dd = df.set_index('last_trade_seconds_ago')[[
+        'taker_volume30d',
+        # 'filler_volume30d',
+     'maker_volume30d']].pipe(np.sign).cumsum().loc[:60*60*24]
+    dd.index /= 3600
+    active_users_past_24hrs = int(dd.values[-1].max())
+    st.plotly_chart(dd.plot(title='# user active over past 24hr = '+str(active_users_past_24hrs)))
         
     
