8f67ed11555009b348d58f5270dd2dbe5cfadaf6
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-02-20 21:26:34 -0500

    liquidity.py: add default value for date

--- liquidity.py
+++ liquidity.py
@@ -236,7 +236,7 @@ def mm_page(clearing_house: ClearingHouse):
     range_selected = molselect.selectbox('range select:', ['daily', 'weekly', 'last month'], 0)
     if range_selected == 'daily':
         lastest_date = pd.to_datetime(slot_to_timestamp_est(latest_slot_full)*1e9, utc=True)
-        date = mol0.date_input('select approx. date:', min_value=datetime.datetime(2022,11,4), max_value=lastest_date) #(datetime.datetime.now(tzInfo)))
+        date = mol0.date_input('select approx. date:', lastest_date, min_value=datetime.datetime(2022,11,4), max_value=lastest_date) #(datetime.datetime.now(tzInfo)))
         values = get_slots_for_date(date)
     elif range_selected == 'weekly':
         values = mol2.slider(
