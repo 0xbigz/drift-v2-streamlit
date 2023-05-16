872c4e9d769dd957eac49d41d16705d583d014fc
0xbigz <83473873+0xbigz@users.noreply.github.com>
2023-05-16 13:39:00 -0400

    fix ALL_MARKET_NAMES

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ ALL_MARKET_NAMES = ["SOL-PERP", "BTC-PERP", "ETH-PERP",
                 'APT-PERP',
                 "1MBONK-PERP", "MATIC-PERP", 
 
-                "ARB-PERP", "DOGE-PERP", 'BNB-PERP', 'SUI-PERP', '1MPEPE-PERP'
+                "ARB-PERP", "DOGE-PERP", 'BNB-PERP', 'SUI-PERP', '1MPEPE-PERP',
                 
                 "SOL"]
