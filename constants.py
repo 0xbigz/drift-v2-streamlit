49fc86826b93944af5214d2766a179c1c2d97da6
0xbigz <83473873+0xbigz@users.noreply.github.com>
2024-07-09 10:15:29 -0400

    update constants.py

--- constants.py
+++ constants.py
@@ -2,5 +2,8 @@ ALL_MARKET_NAMES = ['SOL-PERP', 'BTC-PERP', 'ETH-PERP', 'APT-PERP', '1MBONK-PERP
                      'ARB-PERP', 'DOGE-PERP', 'BNB-PERP', 'SUI-PERP', '1MPEPE-PERP', 'OP-PERP', 
                      'RNDR-PERP', 'XRP-PERP', 'HNT-PERP', 'INJ-PERP', 'LINK-PERP', 'RLB-PERP', 
                      'PYTH-PERP', 'TIA-PERP', 'JTO-PERP', 'SEI-PERP', 'AVAX-PERP', 'WIF-PERP', 'JUP-PERP',
-                     'DYM-PERP', 'TAO-PERP', 'W-PERP'
-                     ] + ["SOL", "mSOL", "wBTC", "wETH", "USDT", 'JitoSOL', 'PYTH', 'JTO', 'bSOL', 'WIF', 'JUP', 'RNDR']
+                     'DYM-PERP', 'TAO-PERP', 'W-PERP', 'KMNO-PERP', 'TNSR-PERP',
+                     'DRIFT-PERP', 'CLOUD-PERP', 'IO-PERP', 'ZEX-PERP', 'POPCAT-PERP',
+                     ] + ["SOL", "mSOL", "wBTC", "wETH", "USDT", 'JitoSOL', 'PYTH', 'JTO', 
+                          'bSOL', 'WIF', 'JUP', 'RNDR', 'W', 'TNSR', 'DRIFT', 
+                          'INF', 'dSOL', 'USDY', 'JLP', 'POPCAT']
